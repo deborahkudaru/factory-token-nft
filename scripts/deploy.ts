203d60528a6b16b30640b4d8f01aad4c9c1fb806
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-03-13 18:20:39 +0100

    deployment scripts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,16 +1,34 @@
-import { ethers } from "hardhat";
+const { ethers } = require("hardhat");
 
 async function main() {
   const Factory = await ethers.getContractFactory("Factory");
 
+  // Deploy the Factory contract
   const factory = await Factory.deploy();
-
   await factory.deployed();
 
-  console.log("Factory contract deployed to:", factory.address);
-  
-}
+  console.log("Factory deployed to:", factory.address);
 
+  // Deploy a new ERC20 token using the Factory
+  const tokenName = "MyToken";
+  const tokenSymbol = "MTK";
+  const tokenSupply = ethers.utils.parseEther("1000000"); 
+
+  const tokenAddress = await factory.deployToken(tokenName, tokenSymbol, tokenSupply);
+  console.log("ERC20 Token deployed to:", tokenAddress);
+
+  const nftName = "MyNFT";
+  const nftSymbol = "MNFT";
+
+  const nftAddress = await factory.deployNFT(nftName, nftSymbol);
+  console.log("NFT deployed to:", nftAddress);
+
+  const tokenCount = await factory.tokenCount();
+  const nftCount = await factory.nftCount();
+
+  console.log("Total Tokens deployed:", tokenCount.toString());
+  console.log("Total NFTs deployed:", nftCount.toString());
+}
 
 main()
   .then(() => process.exit(0))
@@ -18,4 +36,3 @@ main()
     console.error(error);
     process.exit(1);
   });
-
